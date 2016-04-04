849d8b3f7219f7f374e6788eb591837c1e4156f4
ivan6312 <ivan_6312@hotmail.es>
2016-04-03 21:19:25 -0600

    CommiT
    Boton de MOD

---
+++ Calculadora/src/calculadora2/Calculadora2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora2;
+
+/**
+ *
+ * @author Ivan Arroyo
+ */
+public class Calculadora2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    calculadora micalculadora = new calculadora();
+        //indicamos que al abrirse, se ubicará en la mitad de la pantalla al pasarle
+        //el parámetro null
+        micalculadora.setLocationRelativeTo(null);
+        //indicamos que se haga visible
+        micalculadora.setVisible(true);
+}
+}
